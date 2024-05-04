48f5f2297b1b8973a94caa1c0ba9b1e92fd0f65f
Irad Aharoni <irad.aharoni@gmail.com>
2024-05-04 14:01:26 +0300

    Update movie recommendation bot with assistants functionality and vector store

--- apflix/app.py
+++ apflix/app.py
@@ -5,97 +5,110 @@ import streamlit as st
 from openai import OpenAI
 import boto3
 
-MOVIES = [
-    "The Silence of the Lambs",
-    "Pulp Fiction",
-    "The Shawshank Redemption",
-    "Inception",
-    "Jurassic Park",
-    "The Lord of the Rings: The Fellowship of the Ring",
-    "Fight Club",
-    "Titanic",
-    "The Matrix",
-    "Forrest Gump",
-]
-SYSTEM_MESSEGE = f"""You are a movie recommendation bot, You should output JSON with the fields: "title", "reason".
-The movie you recommend should be from the following list: {*MOVIES,}"""
-
-
 def get_secret(secret_id):
+    """
+    Get the secret value from AWS Secrets Manager
+    :param secret_id: The ID of the secret
+    """
     sm = boto3.client('secretsmanager', region_name='us-east-1')
     return sm.get_secret_value(SecretId=secret_id)['SecretString']
 
 def get_movie_data(title):
+    """
+    Get the movie data from title name using the TMDB API
+    :param title: The title of the movie
+    """
     try:
+        # Get the movie data from the title
         response = requests.get(
             f"https://api.themoviedb.org/3/search/movie?api_key={get_secret('TMDB_API_KEY')}&query={title}"
         ).json()
-    except requests.exceptions.RequestException as e:
+    except requests.exceptions.RequestException:
         return
 
+    # Check if the response contains movie results
     if results := response.get("results", []):
         return results[0]
 
 def get_movie_data_from_url(imdb_url):
-    if not (match := re.fullmatch(r"https://www\.imdb\.com/title/(tt\d{7})/", imdb_url)):
+    """
+    Get the movie data from the IMDb URL using the TMDB API
+    :param imdb_url: The IMDb URL of the movie
+    """
+    # Check if the URL is a valid IMDb URL
+    # IMDb URL format: https://www.imdb.com/title/tt1234567/<query_string>
+    if not (match := re.fullmatch(r"https:\/\/www\.imdb\.com\/title\/(tt\d{7,8})\/?(\?[=&\w]*)?", imdb_url)):
         st.error("The URL is not a valid IMDb URL")
         return
     
     imdb_id = match.group(1)
     try:
+        # Get the movie data from the IMDb ID
         response = requests.get(f"https://api.themoviedb.org/3/find/{imdb_id}?api_key={get_secret('TMDB_API_KEY')}&external_source=imdb_id"
             ).json()
-    except requests.exceptions.RequestException as e:
+    except requests.exceptions.RequestException:
         return
 
+    # Check if the response contains movie results
     if results := response.get("movie_results", []):
         return results[0]
 
-def validate_response(response):
+def validate_response(messages):
+    """
+    Validate the assistant's response
+    :param messages: The messages from the assistant
+    """
+    # Check if the response is in a valid JSON format
     try:
-        output = json.loads(response.choices[0].message.content)
+        message = str(messages[0].content[0].text.value)
+        output = json.loads(message)
     except json.JSONDecodeError:
-        st.error("The response is not valid JSON")
+        st.error("The response is not in a valid JSON format")
         return
     
+    # Check if the response contains the correct fields
     if output.keys() != {"title", "reason"}:
         st.error("The response does not contain the correct fields")
         return
     
-    if output["title"] not in MOVIES:
-        st.error("The movie title is not in the list of movies")
-        return
-   
+    # Get the movie data
     if not (data := get_movie_data(output["title"])):
         st.error("The movie data could not be retrieved")
         return
     
+    # Check if the response contains the correct movie data
     if "poster_path" not in data:
         st.error("The response does not contain the poster_path field")
         return
-    
     if "vote_average" not in data:
         st.error("The response does not contain the vote_average field")
         return
     
+    # Add the movie data to the response
     output |= data
     return output
 
 
 def main():
     st.set_page_config(page_title="APFlix", page_icon=":clapper:")
+    # Create a new thread to interact with the assistant
     client = OpenAI(api_key=get_secret('OPENAI_API_KEY'))
-    
+    thread = client.beta.threads.create()
+    # Create a new run in the thread based on user input
     if user_description := st.text_area("Enter User Input"):
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo-0125",
-            response_format={"type": "json_object"},
-            messages=[
-                {"role": "system", "content": SYSTEM_MESSEGE},
-                {"role": "user", "content": user_description},
-            ],
+        client.beta.threads.messages.create(
+            thread_id=thread.id, role="user", content=user_description,
         )
-        if output := validate_response(response):
+        run = client.beta.threads.runs.create_and_poll(
+        thread_id=thread.id,
+        assistant_id="asst_QOfHEUkkGv7aBri9RHpMcxbF",
+        )
+        # Get the assistant's response
+        messeges = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
+
+        # Validate the response
+        if output := validate_response(messeges):
+            # Display the movie data
             col1, col2 = st.columns(2)
             with col1:
                 st.image(f"https://image.tmdb.org/t/p/original{output['poster_path']}")
@@ -104,6 +117,23 @@ def main():
                 st.write(f"**Viewer Ratings**: {output['vote_average']}")
                 st.write(f"### Overview\n{output['overview']}")
                 st.write(f"### Reason\n{output['reason']}")
+
+    # Add a new movie to the vector store
+    with st.expander("Add New Movie :clapper:"):
+        if new_movie := st.text_input("Enter IMDb URL"):
+            # Get the movie data from the URL
+            if data := get_movie_data_from_url(new_movie):
+                with open(f"assets/{data['id']}.json", "w") as f:
+                    json.dump(data, f, indent=4)
+                # Upload the movie data to the vector store
+                movie_file = client.files.create(file=open(f"assets/{data['id']}.json", "rb"), purpose="assistants")
+                client.beta.vector_stores.files.create_and_poll(
+                        vector_store_id="vs_SWvUYppf11XGUVDcQ3LXWU0j",
+                        file_id=movie_file.id
+                        )
+                st.success(f"Added {data['title']} to the list of movies")
+            else:
+                st.error("The movie data could not be retrieved")
 
 
 if __name__ == "__main__":
--- apflix/helper.py
+++ apflix/helper.py
@@ -1,6 +1,7 @@
-from openai import OpenAI
+import os
+import json
 import requests
-import boto3
+from openai import OpenAI
 
 MOVIES = [
     "The Silence of the Lambs",
@@ -14,60 +15,77 @@ MOVIES = [
     "The Matrix",
     "Forrest Gump",
 ]
- 
-def get_secret(secret_id):
-    sm = boto3.client('secretsmanager', region_name='us-east-1')
-    return sm.get_secret_value(SecretId=secret_id)['SecretString']
+
+SYSTEM_MESSEGE = """
+You are a movie recommendation bot.
+Return ONLY a JSON format object (without the prefix ```json and postfix ```) with title and reason fields in your response
+For example (Response format):
+{"title": "The Matrix", "reason": "It's a classic sci-fi movie."}
+Do not include any other information in your response.
+Do not include citations or references in your response.
+"""
+
 
 def get_movie_data(title):
+    """
+    Get the movie data from title name using the TMDB API
+    :param title: The title of the movie
+    """
     try:
+        # Get the movie data from the title
         response = requests.get(
-            f"https://api.themoviedb.org/3/search/movie?api_key={get_secret('TMDB_API_KEY')}&query={title}"
+            f"https://api.themoviedb.org/3/search/movie?api_key={os.get_env('TMDB_API_KEY')}&query={title}"
         ).json()
-    except requests.exceptions.RequestException as e:
+    except requests.exceptions.RequestException:
         return
 
+    # Check if the response contains movie results
     if results := response.get("results", []):
         return results[0]
+
 
 def main():
     client = OpenAI()
     assistant = client.beta.assistants.create(
-    name="Movie Recommendation Bot",
-    instructions="You are a movie recommendation bot, You should output JSON with the fields: \"title\", \"reason\".",
-    model="gpt-3.5-turbo-0125",
-    response_format={"type": "json_object"},
-    tools=[{"type": "file_search"}],
+        name="Movie Recommendation Bot",
+        instructions=SYSTEM_MESSEGE,
+        model="gpt-4-turbo",
+        tools=[{"type": "file_search"}],
     )
 
     # Create a vector store caled "Movies"
     vector_store = client.beta.vector_stores.create(name="Movies")
 
+    # testing the vector store with initial data
+    movie_ids = []
     # Ready the files for upload to OpenAI
     for movie in MOVIES:
         data = get_movie_data(movie)
         if data:
-            client.beta.vector_stores.add_vectors(
-                vector_store_id=vector_store.id,
-                vectors=[{"data": data["title"], "metadata": data}],
-            )
-    file_paths = ["edgar/goog-10k.pdf", "edgar/brka-10k.txt"]
-    file_streams = [open(path, "rb") for path in file_paths]
+            movie_ids.append(data["id"])
+            with open(f"assets/{data['id']}.json", "w") as f:
+                json.dump(data, f, indent=4)
 
+    # Create a list of file streams to upload to OpenAI
+    movie_data_streams = [
+        open(f"assets/{movie_id}.json", "rb") for movie_id in movie_ids
+    ]
     # Use the upload and poll SDK helper to upload the files, add them to the vector store,
     # and poll the status of the file batch for completion.
     file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
-    vector_store_id=vector_store.id, files=file_streams
+        vector_store_id=vector_store.id, files=movie_data_streams
     )
 
-    # You can print the status and the file counts of the batch to see the result of this operation.
-    print(file_batch.status)
     print(file_batch.file_counts)
 
+    # Update the assistant with the vector store
     assistant = client.beta.assistants.update(
-    assistant_id=assistant.id,
-    tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
+        assistant_id=assistant.id,
+        tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
     )
+
+    print(f"{assistant.id} is ready to use the vector store {vector_store.id}")
+
 
 if __name__ == "__main__":
     main()
